15ff2beca078c9c9f2b4545791c6238cc3dffeed
bunda3d <bunda3@yahoo.com>
2026-02-28 13:46:28 -0600

    add navbar login form dropdown

--- Client/src/layout/nav/nav.ts
+++ Client/src/layout/nav/nav.ts
@@ -14,11 +14,20 @@ export class Nav {
   readonly title = signal(inject(APP_TITLE))
   private accountService = inject(AccountService)
   protected creds: any = {}
+  protected isLoggedIn = signal(false);
 
   login() {
     this.accountService.login(this.creds).subscribe({
-      next: result => console.log(result),
+      next: result => {
+        console.log(result);
+        this.isLoggedIn.set(true);
+        this.creds = {}; //reset creds after login
+      },
       error: error => alert(error.message)
     })
+  }
+
+  logout() {
+    this.isLoggedIn.set(false);
   }
 }
