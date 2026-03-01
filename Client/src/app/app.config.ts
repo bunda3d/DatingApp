82a68cc56ceb42469addcbaed841858a2477dc2c
bunda3d <bunda3@yahoo.com>
2026-03-01 16:55:19 -0600

    cleanup

--- Client/src/app/app.config.ts
+++ Client/src/app/app.config.ts
@@ -5,7 +5,7 @@ import {
   provideAppInitializer,
   provideBrowserGlobalErrorListeners,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { InitService } from '../core/services/init-service';
@@ -16,7 +16,7 @@ export const APP_TITLE = new InjectionToken<string>('APP_TITLE');
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
+    provideRouter(routes, withViewTransitions()),
     provideHttpClient(),
     provideAppInitializer(async () => {
       const initService = inject(InitService);
--- Client/src/app/app.ts
+++ Client/src/app/app.ts
@@ -1,11 +1,7 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
-import { lastValueFrom } from 'rxjs';
 import { Nav } from '../layout/nav/nav';
 import { APP_TITLE } from './app.config';
-import { AccountService } from '../core/services/account-service';
-import { User } from '../types/user';
 
 @Component({
   selector: 'app-root',
@@ -13,32 +9,7 @@ import { User } from '../types/user';
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App implements OnInit {
-  private readonly accountService = inject(AccountService);
+export class App {
   protected router = inject(Router);
-  private readonly http = inject(HttpClient);
   protected readonly title = signal(inject(APP_TITLE));
-  protected members = signal<User[]>([]);
-
-  // initialization
-  async ngOnInit() {
-    this.members.set(await this.getMembers());
-    this.setCurrentUser();
-  }
-
-  setCurrentUser() {
-    const userString = localStorage.getItem('user');
-    if (!userString) return;
-    const user = JSON.parse(userString);
-    this.accountService.currentUser.set(user);
-  }
-
-  async getMembers() {
-    try {
-      return lastValueFrom(this.http.get<User[]>('https://localhost:5001/api/members'));
-    } catch (err) {
-      console.error(err);
-      throw err;
-    }
-  }
 }
